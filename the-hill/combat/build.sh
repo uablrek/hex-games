137b8b71077ce590f243fa95802eebebb77d361a
Lars Ekman <uablrek@gmail.com>
2026-04-03 09:11:06 +0200

    the-hill: fix example builds

--- the-hill/combat/build.sh
+++ the-hill/combat/build.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-cp $dir/map-maker/example-map.svg $dir/map-maker/map-data.json $__appd
+hilld=$dir/the-hill
+cp $hilld/map-maker/example-map.svg $hilld/map-maker/map-data.json $__appd
--- the-hill/movement/build.sh
+++ the-hill/movement/build.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-cp $dir/map-maker/example-map.svg $dir/map-maker/map-data.json $__appd
+hilld=$dir/the-hill
+cp $hilld/map-maker/example-map.svg $hilld/map-maker/map-data.json $__appd
 cp $dir/units/hussar.svg $dir/units/unit-gen.js $__appd
